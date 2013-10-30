58d2084110e14a8dd2855b021b4f456d4b589c3a
Nicholas Wargnier <nwargnier@gmail.com>
2013-10-30 15:10:27 -0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+MealPlanner::Application.routes.draw do
+  root to: 'home#index'
+
+  resource :users
+  resource :sessions, only: [:create]
+
+  get '/signup' => 'users#new', as: 'signup'
+  get '/login' => 'sessions#new', as: 'login'
+  delete '/logout' => 'sessions#destroy'
+end
